0745d78ba7c3e2e96333faf2622b13374f11cf22
rishabh arora <rishabharora155@gmail.com>
2023-04-27 13:40:35 +0530

    optimised onr

--- 319bulbSwitcher.cpp
+++ 319bulbSwitcher.cpp
@@ -1,32 +1,15 @@
+// optimised one
 class Solution {
 public:
-    int toggle(int value){
-        if(value==0){
-            return 1;
-        }else{
-            return 0;
-        }
-    }
     int bulbSwitch(int n) {
-        vector<int> arr(n,1);
+        int cnt=0;
+        for(int i=1;i<=n;i++){
 
-        for(int i=1;i<n;i++){
-
-            int  j=i;
-            
-            while(j<n){
-                arr[j]=toggle(arr[j]);
-                j=j+i+1;
-            }
+            int sr=sqrt(i);
+            if(sr*sr==i)cnt++;
 
 
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==1){
-                ans+=1;
-            }
-        }
-        return ans;
+        return cnt;
     }
 };
